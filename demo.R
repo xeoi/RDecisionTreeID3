2abc107aeb8c98d682ee9be69458bb7decd9e244
xeoi <xkli2012@gmail.com>
2017-05-31 07:36:26 +0800

    Update demo.R

--- demo.R
+++ demo.R
@@ -1,4 +1,10 @@
-setwd("/Users/lixukun/Desktop/LearnDecisionTree/")
+#if your folder has structure
+#-DecisionTree
+#--demo.R
+#--data.txt
+#--DecisionTreeDunction.R
+
+setwd("DecisionTree/")
 data = data.matrix(read.table("data.txt"))
 label=data[,1]
 x=data[,-1]
